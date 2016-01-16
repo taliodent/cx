91ae10874fd81619fd5c3ce44e677a8abc889d01
taliodent <kdleyendeckerj@gmail.com>
2016-01-16 13:33:07 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#define INFO "\n Welcome to cx \n ==================================== \t"
+#define VERSION "1.0.0 - January 2016 \n"
+#define NAME "\n cx - simple line based editor \n"
+
+int about(void);
+int readf(void); //DONE
+int appendf(void); //TBD
+int editf(void); //TBD
+int info(void); //TBD
+int concatf(void); //probably an even bigger joke than createf, dire need of overhaul
+int createf(void); // turned out to be fcking joke, needs a rewrite
+
+
