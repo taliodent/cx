6380b10c7eae288e02967cf773cd63674a9ca3de
taliodent <kdleyendeckerj@gmail.com>
2016-01-24 21:41:27 +0100

    added working createf

--- edit.c
+++ edit.c
@@ -53,72 +53,27 @@ int appendf(void)
 
 int createf(void)
 {
-/* createf() creates a new file, allows the user to input information into a 
-* string which will then be written to the file. This should work as the main
-* routine of the program, allowing the user to add as many lines as he wants
-* until he/she sends EOF, which should lead to the routine writing the string
-* into the previously specified file.
-* therefore ther program should ask for a file destination beforehand
-* said file will be either created or overwritten.
-*/
 
-char input[256];
 char destination[256];
-
-/* Needs a huge revamp, most certainly calls for an own implementation of a method that reads
-and stores the input somewhere */
-while (scanf("%s", input) != EOF) {
-scanf("%s", input);
-}
-
 FILE *fp;
-printf("Enter file destination \n");
+char ch;
+printf("Enter file destination:\n");
 scanf("%s", destination);
-	
-	fp = fopen(destination, "w");
-	if (fp == NULL) {
-		perror("Error opening file.");
-	}
-		else {
-		fprintf(fp, "%s", input); /* writes append into file */
-	}
-	fclose(fp);
-
+fp=fopen(destination,"w");
+printf("\nEnter data to be stored in to the file:\n");
+while((ch=getchar())!=EOF)
+putc(ch,fp);
+fclose(fp);
+return 0;
 }
 
-int inputf(void)
-{
-
-char input[256];
-char destination[256];
-int i=0;
+ 
 
 
-while (scanf("%s",  input) != EOF) {
-	while (i=0, i<=255, i++) {
-		scanf("%s", input[i]);
-}
-}
+ 
 
 
 
-FILE *fp;
-printf("Enter file destination \n");
-scanf("%s", destination);
-	if ((destination[0]='/') == 0) { // doesn't work as intended
-		help(); 
-	}
-	else {
-	fp = fopen(destination, "w");
 
-	if (fp == NULL) {
-		perror("Error opening file.");
-	}
-		else {
-		fputs(input, fp);
-		fclose(fp);
-	}
-	
 
-}
-}
+
